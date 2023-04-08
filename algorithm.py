ece06bec21ff2ec715ed1b7e1f7dd294288ba960
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-04-08 19:32:03 +0700

    add astar

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,8 @@
 import heapq
 import Components as comp
+import numpy as np
 
+# Uniform cost search
 def uniform_cost_search(graph: comp.Graph, start, goal):
     pqueue = [(0, (start, []))]     # queue of tuple(cost: int, (nodename: string, path: list of node name))
     explored = set()                # set of explored node
@@ -33,3 +35,46 @@ def uniform_cost_search(graph: comp.Graph, start, goal):
             heapq.heappush(pqueue, (new_cost, (new_node, new_path)))
 
     return 0, []
+
+# A* search
+def a_star(graph: comp.Graph, start, goal):
+    pqueue = [(0, (start, []))]     # queue of tuple(cost: int, (nodename: string, path: list of node name))
+    explored = set()                # set of explored node
+    cameFrom = {}                   # dictionary of node and its parent
+
+    # count g(n)
+    g = [float ('inf') for i in range(len(graph.getListNode()))]
+    g[graph.getIdxNode(start)] = 0
+
+    # count f(n)
+    f = [float ('inf') for i in range(len(graph.getListNode()))]
+    f[graph.getIdxNode(start)] = graph.getNodeWeight(start, goal)
+
+    setHash = {start}
+
+    while (pqueue):
+        cost, (current, tempPath) = heapq.heappop(pqueue)
+
+        # check if current node is goal
+        if current == goal:
+            return cost, tempPath
+
+        # check if current node is explored
+        if current in explored:
+            continue
+        
+        # add current node to explored set
+        explored.add(current)
+
+        # add adjacent node to queue
+        for neighbor in graph.getNeighbor(current):
+            temp_g = g[graph.getIdxNode(current)] + graph.getNodeWeight(current, neighbor)
+            if temp_g < g[graph.getIdxNode(neighbor)]:
+                cameFrom[neighbor] = graph.getIdxNode(current)
+                g[graph.getIdxNode(neighbor)] = temp_g
+                f[graph.getIdxNode(neighbor)] = temp_g + graph.getNodeWeight(neighbor, goal)
+                if neighbor not in setHash:
+                    setHash.add(neighbor)
+                    heapq.heappush(pqueue, (f[graph.getIdxNode(neighbor)], (neighbor, tempPath + [current])))
+    
+    return cost, tempPath
