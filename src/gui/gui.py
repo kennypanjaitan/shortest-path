3e5d99f40d8f2f141327f265e0d9788482dcd389
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-04-12 16:56:23 +0700

    add: show list

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -57,7 +57,9 @@ class UI(QMainWindow):
         # cost
         self.cost = self.findChild(QLabel, "label_10")
         
-        self.label_9 = self.findChild(QLabel, "label_9")
+        # showmap
+        self.map = self.findChild(QPushButton, "pushButton_5")
+        self.map.clicked.connect(self.showmap)
 
         # path
         self.path = self.findChild(QLabel, "label_11")
@@ -163,6 +165,43 @@ class UI(QMainWindow):
                 pass
             elif self.dropdown.currentText() == "Cilacap":
                 pass
+    #show map
+    def showmap(self):
+        if self.dropdown.currentText() == "ITB Ganesha":
+            self.widget = QWidget()
+            self.widget.resize(800, 600)
+            label = QLabel(self.widget)
+            Gane = "ITB Ganesha\n 1. Gerbang Utama\n 2. Gedung Sipil\n 3. Gedung CIBE\n 4. CC Barat\n 5. CC Timur\n 6. Lapangan Cinta\n 7. Gedung SAPPK\n 8. Gedung SR \n 9. Sekretariat IMG\n 10. Gedung Lingkungan\n 11. GKU Barat\n 12. Labtek VII\n 13. Labtek III\n 14. Gedung Kimia\n 15. Gedung FTTM\n 16. Gedung FITB\n 17. Gedung CAS\n 18. Gedung CRCS\n 19. Perpustakaan\n 20. Gedung SBM\n 21. Sunken Court\n 22. Pusat Gema\n 23. Plaza Widya\n 24. Aula Barat-Timur"
+            label.setText(str(Gane))
+            self.widget.setWindowTitle("ITB Ganesha")
+            self.widget.show()
+        elif self.dropdown.currentText() == "ITB Jatinangor":
+            self.widget = QWidget()
+            self.widget.resize(500, 300)
+            label = QLabel(self.widget)
+            Nangor = "ITB Jatinangor\n 1. Gerbang Utama\n 2. Bundaran\n 3. GKU\n 4. Water Treatment Plan\n 5. Asrama Mahasiswa\n 6. GSG\n 7. Koica\n 8. Rektorat\n 9. GKU 1\n 10. Asrama Dosen"
+            label.setText(str(Nangor))
+            self.widget.setWindowTitle("ITB Jatinangor")
+            self.widget.show()
+        elif self.dropdown.currentText() == "Alun-Alun Bandung":
+            self.widget = QWidget()
+            self.widget.resize(800, 600)
+            label = QLabel(self.widget)
+            Alun = "Alun-Alun Bandung\n 1. Paskal\n 2. Parapan Kebon Jati\n 3. Sudirman\n 4. Stasiun Bandung\n 5. Cibadak\n 6. Astana Anyar\n 7. Pasir Koja\n 8. Norsefiicden\n 9. Otto Iskandar Dinata\n 10. Pungkur\n 11. Patung Tentara Pelajar\n 12. Braga Citywalk\n 13. Braga\n 14. Air Mancur Asia Afrika\n 15. Jalan ABC\n 16. Asia Afrika\n 17. Mural Asia Afrika\n 18. Jalan Homan\n 19. Alun-Alun Bandung\n 20. Pendopo Kota Bandung\n 21. Pasar Baru"
+            label.setText(str(Alun))
+            self.widget.setWindowTitle("Alun-Alun Bandung")
+            self.widget.show()
+        elif self.dropdown.currentText() == "Bandung Selatan":
+            pass
+        elif self.dropdown.currentText() == "Cilacap":
+            pass
+        elif self.dropdown.currentText() == "Input File":
+            self.widget = QWidget()
+            self.widget.resize(100,100)
+            label = QLabel(self.widget)
+            label.setText("You Choose Input File")
+            self.widget.setWindowTitle("Input File")
+            self.widget.show()
 
     #ucs
     def chooseUCS(self):
